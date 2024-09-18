2d8e858c2abd66fd1215756303fae55f4b9b4222
ian-lundy27 <ian.lundy@student.fairfield.edu>
2024-09-18 12:02:41 -0400

    prototype main function stuff for taking file content and reducing it to individual words, and logic for counting them

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        File file = new File("project-information.txt");
+        File file = new File("media/project-information.txt");
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -14,8 +15,30 @@ public class Main {
         while ((in = br.readLine()) != null) {
             text.append(in).append("\n");
         }
-        
+
         System.out.println(text);
+
+        String strippedText = text.toString().replaceAll("[^A-Za-z0-9]"," ").replaceAll(" +"," ").toLowerCase().trim();
+        System.out.println(strippedText);
+
+        String[] wordList = strippedText.split(" ");
+        System.out.println(wordList.length);
+        System.out.println(Arrays.toString(wordList));
+
+        HashMap<String,Integer> wordMap = new HashMap<String,Integer>();
+
+        for (String word : wordList) {
+
+            if (!wordMap.containsKey(word)) {
+                wordMap.put(word, 1);
+            } else {
+                wordMap.put(word, wordMap.get(word) + 1);
+            }
+
+        }
+
+        System.out.println(wordMap.toString());
+
 
 
     }
