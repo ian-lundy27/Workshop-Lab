a38533ac63566237c183fd7735af8d1d0c2e86e0
ian-lundy27 <ian.lundy@student.fairfield.edu>
2024-10-23 11:55:38 -0400

    Add method to SentimentAnalysis that rates article on positivity/negativity

--- src/SentimentAnalysis.java
+++ src/SentimentAnalysis.java
@@ -1,2 +1,18 @@
+import java.util.HashMap;
+
 public class SentimentAnalysis {
+
+    static HashMap<String,Integer> polarityMap;
+
+    public SentimentAnalysis() {
+    }
+
+    public static int getArticlePolarity(Article article) {
+        int polarity = 0;
+        for (String word : article.wordList) {
+            if (polarityMap.containsKey(word)) polarity += polarityMap.get(word);
+        }
+        return polarity;
+    }
+
 }
